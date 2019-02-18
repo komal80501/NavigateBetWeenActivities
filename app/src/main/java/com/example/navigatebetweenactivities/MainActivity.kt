c931b150e4df8926456c4ce77f6782734a07a98e
Hp <komalmane48@gmail.com>
2019-02-18 18:59:11 +0530

    Navigation Activity

---
+++ app/src/main/java/com/example/navigatebetweenactivities/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.navigatebetweenactivities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSend.setOnClickListener {
+
+            val message:String = editTextMsg.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+
+
+        }
+    }
+}
